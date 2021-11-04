0b0d768c96b3652465e33250f391feea33f28cd9
Benjamin Lindley <91563232+lindleyb@users.noreply.github.com>
2021-11-04 18:55:46 +0000

    Doe

--- student.py
+++ student.py
@@ -64,10 +64,10 @@ class Piggy(PiggyParent):
         self.turn_by_deg(90)
       
     def doe(self):
-      self.deg_fwd(360)
-      self.deg_turn(90)
-      self.deg_fwd(360)
-      self.deg_turn(90)
+      self.fwd(360)
+      self.turn_right(90)
+      self.fwd(360)
+      self.turn_right(90)
     def dance(self):
         """A higher-ordered algorithm to make your robot dance"""
         # TODO: check to see if it's safe before dancing
