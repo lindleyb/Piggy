f1ec0f93e8ad36c5bf0c143acf7503a396aad2da
Benjamin Lindley <91563232+lindleyb@users.noreply.github.com>
2021-11-18 19:16:33 +0000

    changed motor power

--- student.py
+++ student.py
@@ -16,7 +16,7 @@ class Piggy(PiggyParent):
         ''' 
         MAGIC NUMBERS <-- where we hard-code our settings
         '''
-        self.LEFT_DEFAULT = 80
+        self.LEFT_DEFAULT = 73
         self.RIGHT_DEFAULT = 80
         self.MIDPOINT = 1475  # what servo command (1000-2000) is straight forward for your bot?
         self.set_motor_power(self.MOTOR_LEFT + self.MOTOR_RIGHT, 0)
