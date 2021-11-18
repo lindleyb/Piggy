52e5b32c365178d9e3cc2b13dc32cb51afe3320d
Benjamin Lindley <91563232+lindleyb@users.noreply.github.com>
2021-11-18 19:49:09 +0000

    added the which way is closer part

--- student.py
+++ student.py
@@ -68,7 +68,25 @@ class Piggy(PiggyParent):
         self.fwd()
         self.read_distance()
         if self.read_distance() < 100:
-          self.servo
+          self.servo(1075)
+          if self.read_distance > 100:
+            self.right()
+            time.sleep(0.75)
+            self.stop()
+            self.fwd()
+            time.sleep(3)
+            self.left()
+            time.sleep(0.75)
+          else if self.read_distance < 100:
+            self.servo(1875)
+            if self.read_distance > 100:
+              self.right()
+              time.sleep(0.75)
+              self.stop()
+              self.fwd()
+              time.sleep(3)
+              self.left
+              time.sleep(0.75)
 
     def dance(self):
       while True:
